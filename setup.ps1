d7cd06af0220d0e085878311e6ca9b7763c4befb
bomburr <dc.akyildiz@gmail.com>
2021-12-04 22:38:20 +0300

    vs settings

--- setup.ps1
+++ setup.ps1
@@ -1,14 +1,19 @@
 $userpath = $env:HOMEPATH
 Write-Host "Choco is installing..." -ForegroundColor Green
-Set-ExecutionPolicy Bypass -Scope Process -Force; iwr https://community.chocolatey.org/install.ps1 -UseBasicParsing | iex,
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+
+choco install vmware-workstation-player
 choco install git  -y
 choco install vscode -y
 choco install python -y
 choco install lightshot.install  -y
 choco install spotify  -y
 choco install winscp  -y
-choco install oh-my-posh -y 
+choco install oh-my-posh -y
+choco install everything -y 
+
+Invoke-WebRequest -Uri"https://raw.githubusercontent.com/bomburr/dotfiles/main/night-owl.omp.json" -OutFile "c:$user\Documents\WindowsPowerShell\Modules\oh-my-posh\6.17.0\themes\night-owl.omp.json"
 
 ## editing powershell profile
 get-item "c:$user\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" | Add-Content -Value 'Import-Module oh-my-posh
@@ -24,6 +29,7 @@ $envs.GetEnumerator() | % {$Env:Path += ";$($_)"}
 ## editing powershell profile
 get-item "c:$user\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" | Add-Content -Value 'Import-Module oh-my-posh
 Set-PoshPrompt -Theme night-owl'
+
 
 ## git clones 
 
