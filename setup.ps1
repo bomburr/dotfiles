cb0f6800f91e5030e1f6b2bc5e197ab810beaf11
bomburr <dc.akyildiz@gmail.com>
2021-12-05 00:09:45 +0300

    path fixed

--- setup.ps1
+++ setup.ps1
@@ -13,27 +13,27 @@ choco install spotify  -y
 choco install winscp  -y
 choco install oh-my-posh -y
 choco install everything -y 
+
 Write-host "oh-my-posh Template installing" -ForegroundColor Green
-Invoke-WebRequest -Uri"https://raw.githubusercontent.com/bomburr/dotfiles/main/robbyrussel.omp.json" -OutFile "c:$user\Documents\WindowsPowerShell\Modules\oh-my-posh\6.17.0\themes\robbyrussel.omp.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bomburr/dotfiles/main/robbyrussel.omp.json" -OutFile "c:$userpath\Documents\WindowsPowerShell\Modules\oh-my-posh\6.17.0\themes\robbyrussel.omp.json"
 Write-host "editing settings.json for vscode" -ForegroundColor Green
-Invoke-WebRequest -Uri"https://raw.githubusercontent.com/bomburr/dotfiles/main/settings.json" -OutFile "c:$user\AppData\Roaming\Code\User\settings.json"
+Invoke-WebRequest -Uri "https://raw.githubusercontent.com/bomburr/dotfiles/main/settings.json" -OutFile "c:$userpath\AppData\Roaming\Code\User\settings.json"
 
 ## editing powershell profile 
-get-item "c:$user\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" | Add-Content -Value 'Import-Module oh-my-posh
+get-item "c:$userpath\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" | Add-Content -Value 'Import-Module oh-my-posh
 Set-PoshPrompt -Theme robbyrussel
 Import-Module -Name Terminal-Icons'
 
 ## env variables
-$envs = @("%USERPROFILE%\AppData\Local\Programs\oh-my-posh\bin",
-          "%USERPROFILE%\AppData\Local\Programs\oh-my-posh\themes")
+$envs = @("%USERPROFILE%\AppData\Local\Programs\oh-my-posh\bin")
 
 $envs.GetEnumerator() | % {$Env:Path += ";$($_)"}
 
 ## git clones 
-
+if(-not (get-item c:\works)){
 if(new-item -Name "works" -Path "C:\" -ItemType Directory){
     new-item -Name "repos" -Path "C:\works" -ItemType Directory
-}
+}}
 
 $git_base = "C:\works\repos"
 
